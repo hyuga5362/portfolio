3f9ba21784b683fc1c01efeabedc16870d3aefca
ishikawa <bibichan5362@gmail.com>
2025-05-25 14:39:12 +0900

    portforio

--- start-folder/JavaScriptWork/work1_3/1_3.js
+++ start-folder/JavaScriptWork/work1_3/1_3.js
@@ -2,10 +2,10 @@ const getTotalPrice = (price, quantity) => price * quantity;
 
 const addTax = total => total * 1.1;
 
-const total = getTotalPrice(1000, 2); // → 2000
+const total = getTotalPrice(1000, 2);
 
 console.log(`税抜金額は${total}円です`);
 
-const taxedTotal = addTax(total);     // → 2200
+const taxedTotal = addTax(total);
 
 console.log(`税込金額は${taxedTotal}円です`);
