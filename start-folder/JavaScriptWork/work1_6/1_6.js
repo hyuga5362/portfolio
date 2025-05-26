61e2c4887c12327d7d449be650509cb6fe6fa765
ishikawa <bibichan5362@gmail.com>
2025-05-26 13:33:30 +0900

    week1_6

---
+++ start-folder/JavaScriptWork/work1_6/1_6.js
@@ -0,0 +1,52 @@
+console.log("お客さんがコーヒーとサンドイッチを注文しました");
+
+
+function makeCoffee() {
+
+    return new Promise((resolve) => {
+        console.log("☕ スタッフ：先にコーヒーを準備します...")
+
+      setTimeout(() => {
+
+        console.log("✅ コーヒーができました！");
+
+        resolve("コーヒー");
+
+      }, 2000); // 2秒
+
+    });
+
+  }
+
+
+
+  function makeSandwich() {
+
+    
+    return new Promise((resolve) => {
+        console.log("🥪 スタッフ：次にサンドイッチを準備します..");
+
+      setTimeout(() => {
+
+        console.log("✅ サンドイッチができました！");
+
+        resolve("サンドイッチ");
+
+      }, 3000); // 3秒
+
+    });
+
+  }
+
+
+
+  async function serveCustomer() {
+
+    await Promise.all([makeCoffee(),makeSandwich()]);
+
+    console.log("🍽️ スタッフ：すべてできたので配膳します！");
+
+  }
+
+
+  serveCustomer();
